f087233e0de845d40cd0f310b00c991239e4956d
Nguyen Quang(Quinn)/ Sotatek/ MnF iOS <e.quang.nguyen4@wesang.com>
2024-05-30 16:57:39 +0700

    Initial Commit

---
+++ MindPlex/MindPlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindPlexApp.swift
+//  MindPlex
+//
+//  Created by Nguyễn Như Quang on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MindPlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
